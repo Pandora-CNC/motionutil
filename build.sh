57491c4a8679627f0628c0a4b425b290a066cd74
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-26 21:20:40 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# motionutil build script
-# (c) 2017 0xbmuessig
-
-rm -f mutil*.elf
-arm-none-linux-gnueabi-gcc -static mutil.c -o mutil.elf
-arm-none-linux-gnueabi-strip --strip-unneeded -o mutil.min.elf mutil.elf
