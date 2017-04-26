deb2ca58880890bb701c90b4c5d1477bb9d3184f
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-26 21:17:15 +0200

    Changed line endings

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# motionutil build script
+# (c) 2017 0xbmuessig
+
 rm -f mutil*.elf
 arm-none-linux-gnueabi-gcc -static mutil.c -o mutil.elf
 arm-none-linux-gnueabi-strip --strip-unneeded -o mutil.min.elf mutil.elf
